cec4a69340c0b8d1f3b35ca931e990f339e19255
Maria Gabriela <mgtmc@cin.ufpe.br>
2016-06-14 16:03:46 -0300

    First commit

---
+++ DeadlockException.java
@@ -0,0 +1,16 @@
+package locks;
+
+public class DeadlockException extends RuntimeException {
+
+    public DeadlockException(Throwable t) {
+        super(t);
+    }
+
+    public DeadlockException() {
+        super();
+    }
+
+    public DeadlockException(String st) {
+        super(st);
+    }
+}
